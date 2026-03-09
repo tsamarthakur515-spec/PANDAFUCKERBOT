539735837ff10403934f9a2ec6ded0a37f24de3e
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-09 20:56:49 +0545

    Update leave.py

--- AltBots/modules/leave.py
+++ AltBots/modules/leave.py
@@ -29,7 +29,7 @@ async def leave(e):
                   alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
                   await e.reply(alt)
              else:
-                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+                  event = await e.reply("» ᴍᴀɪɴ ᴊʜᴀ ʀᴀʜᴀ ʜᴜ 🙂 ")
                   try:
                       await event.client(LeaveChannelRequest(int(e.chat_id)))
                   except Exception as e:
