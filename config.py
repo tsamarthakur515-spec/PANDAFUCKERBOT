da74282106bb3381a5b16989b733343a70ee3bc5
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-08-25 10:53:35 +0545

    Set OWNER_ID to 8711056379

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ BOT_TOKENS = [
 SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="7311297618").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="7450385463"))
+OWNER_ID = int(getenv("OWNER_ID", default="8711056379"))
 SUDO_USERS.append(OWNER_ID)
 
 # How many bots to start by default
