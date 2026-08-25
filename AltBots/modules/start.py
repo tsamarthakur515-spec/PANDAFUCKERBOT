cd9495c5a2bfa907054258af6cfec3b9b73acb6b
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-08-25 12:04:02 +0545

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -4,8 +4,8 @@ from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
 START_BUTTON = [
     [Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/+4eTddUofQDlkYjhl"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/suruchi_network")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/Swastika_update"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/+Mm4ndifeQ_cxMmQ1")
     ],
     [Button.inline("• ʀᴇᴘᴏ •", data="repo")]
 ]
@@ -18,7 +18,7 @@ async def start_handler(event):
     bot_name = AltBot.first_name
     bot_id = AltBot.id
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}]\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [꯭𐏓꯭🇳🇵꯭𐏓꯭ 𝐀꯭𝛅 ꯭ꭙ ꯭ᯓ꯭𓆰𝅃꯭᳚ ⃪ ⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠༎ ꯭𝐒꯭𝛂꯭𝐦꯭𝐚𝐫 ꯭꯭𝆺꯭𝅥༎ࠫ𐏓꯭꯭𝅥🍃꯭](tg://openmessage?user_id=7724452546)**\n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [⎯꯭‌𝅃꯭꯭꯭᳚ ꯭𓆰꯭꯭🍃꯭♔꯭𝐑꯭𝛕꯭꯭֟ؖ۬፝𝛅༭꯭𝐉ᴀ꯭ᴍ꯭፝֟፝֟ᴇ꯭s꯭𝄢꯭|꯭🔥꯭꯭➛](tg://openmessage?user_id=8841848847)**\n\n"
     TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
     TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
@@ -36,7 +36,7 @@ async def button_handler(event):
     
     if data == "repo":
         await event.answer(
-            "𝐁𝐇𝐀𝐆 𝐉𝐇𝐀 𝐌𝐀𝐃𝐄𝐑𝐂𝐇𝐎𝐃 𝐑𝐄𝐏𝐎 𝐊𝐘𝐀 𝐋𝐀𝐆𝐀 𝐒𝐀𝐌𝐀𝐑 𝐓𝐇𝐀𝐊𝐔𝐑 𝐊𝐀 𝐋𝐀𝐍𝐃 𝐋𝐄𝐋𝐄 👿\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ ˹ᴀʀᴜ × ᴀᴘɪ˼ × [ʙᴏᴛs]",
+            "ʙʜᴀɢ ᴊᴀ ʙʜɪᴋʜᴀʀɪ, ᴋʜᴜᴅ sᴇ ʙᴀɴᴀ ʟᴇ ʀᴇᴘᴏ 😐\n\n ᴘᴏᴡᴇʀᴇᴅ ʙʏ ˹ᴀʀᴜ × ᴀᴘɪ˼ × [ʙᴏᴛs]",
             alert=True
         )
 
